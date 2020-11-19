b7fbba36475c8f28f03c5709ba89d90f7a25ce8e
luopengfei3000 <luopengfei3000@163.com>
2020-11-19 15:11:36 +0800

    提交到dev分支

--- cloud-provider-payment8001/src/main/java/com/luopf/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/luopf/springcloud/controller/PaymentController.java
@@ -14,40 +14,35 @@ import javax.annotation.Resource;
  */
 @RestController
 @Slf4j
-public class PaymentController
-{
+public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
     @GetMapping(value = "/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
     }
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(@RequestBody Payment payment)
-    {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
-        log.info("*****插入结果："+result);
+        log.info("*****插入结果：" + result);
 
-        if(result > 0)
-        {
-            return new CommonResult(200,"插入数据库成功,serverPort: ");
-        }else{
-            return new CommonResult(444,"插入数据库失败",null);
+        if (result > 0) {
+            return new CommonResult(200, "插入数据库成功,serverPort: ");
+        } else {
+            return new CommonResult(444, "插入数据库失败", null);
         }
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
-    {
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
 
-        if(payment != null)
-        {
-            return new CommonResult(200,"查询成功,serverPort:  "+payment);
-        }else{
-            return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
+        if (payment != null) {
+            return new CommonResult(200, "查询成功,serverPort:  " + payment);
+        } else {
+            return new CommonResult(444, "没有对应记录,查询ID: " + id, null);
         }
     }
 
